27882fe487195e9c05b5187e1206a3ef86a89cb3
Aleksandr-Prodan <aleksandrprodan@ukr.net>
2025-10-21 20:24:39 +0000

    Deploying to gh-pages from @ Aleksandr-Prodan/goit-js-hw-11@1553ebeb49fd8b3597faffa9e7b90e3d5437db74 🚀

--- index.js
+++ index.js
@@ -1,9 +1,14 @@
-import{i as c}from"./assets/vendor-I1I71QQ2.js";(function(){const t=document.createElement("link").relList;if(t&&t.supports&&t.supports("modulepreload"))return;for(const e of document.querySelectorAll('link[rel="modulepreload"]'))n(e);new MutationObserver(e=>{for(const r of e)if(r.type==="childList")for(const s of r.addedNodes)s.tagName==="LINK"&&s.rel==="modulepreload"&&n(s)}).observe(document,{childList:!0,subtree:!0});function o(e){const r={};return e.integrity&&(r.integrity=e.integrity),e.referrerPolicy&&(r.referrerPolicy=e.referrerPolicy),e.crossOrigin==="use-credentials"?r.credentials="include":e.crossOrigin==="anonymous"?r.credentials="omit":r.credentials="same-origin",r}function n(e){if(e.ep)return;e.ep=!0;const r=o(e);fetch(e.href,r)}})();const a=document.querySelector(".form");a.addEventListener("submit",i=>{i.preventDefault(),fetch("https://pixabay.com/api/?key=52857453-560792bcb66dbe1ba9d4bb66c&q=query&image_type=photo&orientation=horizontal&safesearch=true").then(t=>t.json()).then(t=>{const o=u(t.hits);document.querySelector(".gallery").innerHTML=o}).catch(t=>{c.error({message:"Sorry, there are no images matching your search query. Please try again!",position:"topRight"})})});function u(i){return i.map(({webformatURL:t,tags:o,likes:n})=>`
-        <div class="photo-card">
-          <img src="${t}" alt="${o}" loading="lazy" />
-          <div class="info">
-            <p><b>Likes:</b> ${n}</p>
-          </div>
-        </div>
-      `).join("")}
+import{a as p,S as m,i as a}from"./assets/vendor-BNibzuFn.js";(function(){const r=document.createElement("link").relList;if(r&&r.supports&&r.supports("modulepreload"))return;for(const e of document.querySelectorAll('link[rel="modulepreload"]'))n(e);new MutationObserver(e=>{for(const t of e)if(t.type==="childList")for(const s of t.addedNodes)s.tagName==="LINK"&&s.rel==="modulepreload"&&n(s)}).observe(document,{childList:!0,subtree:!0});function o(e){const t={};return e.integrity&&(t.integrity=e.integrity),e.referrerPolicy&&(t.referrerPolicy=e.referrerPolicy),e.crossOrigin==="use-credentials"?t.credentials="include":e.crossOrigin==="anonymous"?t.credentials="omit":t.credentials="same-origin",t}function n(e){if(e.ep)return;e.ep=!0;const t=o(e);fetch(e.href,t)}})();const y="https://pixabay.com/api/",g="52857453-560792bcb66dbe1ba9d4bb66c";async function h(i){const r={key:g,q:i,image_type:"photo",orientation:"horizontal",safesearch:!0};return(await p.get(y,{params:r})).data}let l;function b(i){const r=document.querySelector(".gallery"),o=i.map(({webformatURL:n,largeImageURL:e,tags:t,likes:s,views:u,comments:d,downloads:f})=>`
+    <li class="gallery-item">
+      <a class="gallery-link" href="${e}">
+        <img class="gallery-image" src="${n}" alt="${t}" />
+      </a>
+      <div class="info">
+        <p><b>Likes:</b> ${s}</p>
+        <p><b>Views:</b> ${u}</p>
+        <p><b>Comments:</b> ${d}</p>
+        <p><b>Downloads:</b> ${f}</p>
+      </div>
+    </li>
+  `).join("");r.insertAdjacentHTML("beforeend",o),l?l.refresh():l=new m(".gallery a")}function L(){document.querySelector(".gallery").innerHTML=""}function S(){document.querySelector("#loader").classList.remove("hidden")}function q(){document.querySelector("#loader").classList.add("hidden")}const c=document.querySelector(".form"),w=c.querySelector("input");c.addEventListener("submit",async i=>{i.preventDefault();const r=w.value.trim();if(!r){a.warning({title:"Warning",message:"Please enter a search term!",position:"topRight"});return}L(),S();try{const o=await h(r);o.hits.length===0?a.info({title:"No results",message:"Sorry, there are no images matching your search query. Please try again!",position:"topRight"}):b(o.hits)}catch(o){a.error({title:"Error",message:"Something went wrong. Please try again later.",position:"topRight"}),console.error(o)}finally{q()}});
 //# sourceMappingURL=index.js.map
