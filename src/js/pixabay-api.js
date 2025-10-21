1553ebeb49fd8b3597faffa9e7b90e3d5437db74
oleksandr <aleksandrprodan@ukr.net>
2025-10-21 23:24:06 +0300

    end

--- src/js/pixabay-api.js
+++ src/js/pixabay-api.js
@@ -1,42 +1,19 @@
-import iziToast from "izitoast";
-import "izitoast/dist/css/iziToast.min.css";
+import axios from 'axios';
 
-// const { default: axios } = require("axios")
+const BASE_URL = "https://pixabay.com/api/";
 
-const form = document.querySelector('.form');
+const API_KEY = "52857453-560792bcb66dbe1ba9d4bb66c";
 
-form.addEventListener('submit', (event) => {
-  event.preventDefault();
-
-  fetch('https://pixabay.com/api/?key=52857453-560792bcb66dbe1ba9d4bb66c&q=query&image_type=photo&orientation=horizontal&safesearch=true')
-    .then((response) => {
-      // обробка успішного запиту
-      return response.json()
-    })
-    .then(data => {
-      const markup = getImagesByQuery(data.hits);
-      document.querySelector('.gallery').innerHTML = markup;
-    })
-    .catch((error) => {
-      // обробка помилки
-      iziToast.error({
-        message: `Sorry, there are no images matching your search query. Please try again!`,
-        position: 'topRight',
-      })
-    })
-});
-
-function getImagesByQuery(images) {
-    return images
-      .map(({ webformatURL, tags, likes }) => {
-        return `
-        <div class="photo-card">
-          <img src="${webformatURL}" alt="${tags}" loading="lazy" />
-          <div class="info">
-            <p><b>Likes:</b> ${likes}</p>
-          </div>
-        </div>
-      `;
-      })
-      .join('');
+export async function getImagesByQuery(query) {
+  const params = {
+    key: API_KEY,             
+    q: query,
+    image_type: 'photo',
+    orientation: 'horizontal',
+    safesearch: true,
   };
+
+  const response = await axios.get(BASE_URL, { params });
+
+  return response.data;
+}
--- src/js/render-functions.js
+++ src/js/render-functions.js
@@ -1,12 +1,54 @@
-function createGallery(images) {
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
 
-};
-function clearGallery() {
+let lightbox;
 
-};
-function showLoader() {
+export function createGallery(images) {
+  const galleryEl = document.querySelector('.gallery');
 
-};
-function hideLoader() {
+  const markup = images
+    .map(
+      ({
+        webformatURL,
+        largeImageURL,
+        tags,
+        likes,
+        views,
+        comments,
+        downloads,
+      }) => `
+    <li class="gallery-item">
+      <a class="gallery-link" href="${largeImageURL}">
+        <img class="gallery-image" src="${webformatURL}" alt="${tags}" />
+      </a>
+      <div class="info">
+        <p><b>Likes:</b> ${likes}</p>
+        <p><b>Views:</b> ${views}</p>
+        <p><b>Comments:</b> ${comments}</p>
+        <p><b>Downloads:</b> ${downloads}</p>
+      </div>
+    </li>
+  `
+    )
+    .join('');
 
-};
+  galleryEl.insertAdjacentHTML('beforeend', markup);
+
+  if (!lightbox) {
+    lightbox = new SimpleLightbox('.gallery a');
+  } else {
+    lightbox.refresh();
+  }
+}
+
+export function clearGallery() {
+  document.querySelector('.gallery').innerHTML = '';
+}
+
+export function showLoader() {
+  document.querySelector('#loader').classList.remove('hidden');
+}
+
+export function hideLoader() {
+  document.querySelector('#loader').classList.add('hidden');
+}
